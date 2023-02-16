0cf8e5a6132c001007486a3e78663b456b3d43d0
80Quattro <muzyczek2@gmail.com>
2023-02-16 10:47:37 +0100

    Improve showing all urls

--- resources/views/url/index.blade.php
+++ resources/views/url/index.blade.php
@@ -1,1 +1,26 @@
-{{$urls}}
+@unless(count($urls) == 0)
+
+<table>
+    <thead>
+        <tr>
+            <th>Short URL</th>
+            <th>Destination URL</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($urls as $url)
+            <tr>
+                <td>{{$url['default_short_url']}}</td>
+                <td>{{$url['destination_url']}}</td>
+                <td><a href="/urls/{{$url['id']}}">Details</a></td>
+                <td><a href="/urls/{{$url['id']}}/edit">Edit</a></td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@else
+
+You have no URLs yet.
+
+@endunless
