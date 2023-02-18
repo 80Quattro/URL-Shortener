de785675c015e2097f3a574c0c347d4d5b29f2c1
80Quattro <muzyczek2@gmail.com>
2023-02-18 13:49:10 +0100

    Protect routes (user and guset)

--- routes/web.php
+++ routes/web.php
@@ -22,40 +22,51 @@ Route::get('/', function () {
 });
 
 // Show All URLs
-Route::get('/urls', [URLController::class, 'index']);
+Route::get('/urls', [URLController::class, 'index'])
+    ->middleware('auth');
 
 // Show Create URL Form
-Route::get('/urls/create', [URLController::class, 'create']);
+Route::get('/urls/create', [URLController::class, 'create'])
+    ->middleware('auth');
 
 // Store URL Data
-Route::post('/urls', [URLController::class, 'store']);
+Route::post('/urls', [URLController::class, 'store'])
+    ->middleware('auth');
 
 // Show Single URL
-Route::get('/urls/{url}', [URLController::class, 'show']);
+Route::get('/urls/{url}', [URLController::class, 'show'])
+    ->middleware('auth');
 
 // Show Edit URL Form
-Route::get('/urls/{url}/edit', [URLController::class, 'edit']);
+Route::get('/urls/{url}/edit', [URLController::class, 'edit'])
+    ->middleware('auth');
 
 // Update URL
-Route::put('/urls/{url}', [URLController::class, 'update']);
+Route::put('/urls/{url}', [URLController::class, 'update'])
+    ->middleware('auth');
 
 // Delete URL
-Route::delete('/urls/{url}', [URLController::class, 'destroy']);
+Route::delete('/urls/{url}', [URLController::class, 'destroy'])
+    ->middleware('auth');
 
 // Redirect shortURLKey to Destination URL
 Route::get('/s/{shortURLKey}', \AshAllenDesign\ShortURL\Controllers\ShortURLController::class);
 
 // Show Register Form
-Route::get('/register', [UserController::class, 'create']);
+Route::get('/register', [UserController::class, 'create'])
+    ->middleware('guest');
 
 // Create New User
 Route::post('/users', [UserController::class, 'store']);
 
 //Show Login Form
-Route::get('/login', [UserController::class, 'login']);
+Route::get('/login', [UserController::class, 'login'])
+    ->name('login')
+    ->middleware('guest');
 
 // User Authenticate
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
 
 // User Logout
-Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/logout', [UserController::class, 'logout'])
+    ->middleware('auth');
