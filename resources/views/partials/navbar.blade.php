ae53ee2d8f335aea33a62b8e18b2c5a4fffe8a85
80Quattro <muzyczek2@gmail.com>
2023-02-21 13:44:21 +0100

    Add Nav styles

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,14 @@
-<a href="/">Home</a>
-
-@auth
-    @include('partials.user-nav')
-@else
-    @include('partials.guest-nav')
-@endauth
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">URL Shortener</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        @auth
+            @include('partials.user-nav')
+        @else
+            @include('partials.guest-nav')
+        @endauth
+    </div>
+</nav>
--- resources/views/partials/user-nav.blade.php
+++ resources/views/partials/user-nav.blade.php
@@ -1,8 +1,14 @@
-<ul>
-    <li><a href="/urls">Your URLs</a></li>
-    <li><a href="/urls/create">Create new</a></li>
-</ul>
-<form method="POST" action="/logout">
-    @csrf
-    <button>Logout</button>
-</form>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+            <a class="nav-link" href="/urls">Your URLs</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/urls/create">Create new</a>
+        </li>
+    </ul>
+    <form method="POST" action="/logout" class="d-flex">
+        @csrf
+        <button>Logout</button>
+    </form>
+</div>
